a64361de8fc135a75f5bf4d8a57bab3a23e52ccc
Javan R <mail@javan.de>
2019-08-09 10:52:18 +0200

    added navbar

--- ui.R
+++ ui.R
@@ -1,63 +1,61 @@
 library(shiny)
 
-shinyUI(fluidPage(
-  
-  # Application title
-  titlePanel("Bienenstand Shiny App"),
-  
-  sidebarLayout(
-    sidebarPanel(
-      p(h4("Eigenes File hochladen")),
-      fileInput("fileuploadFile", "CSV-Datei auswählen:",
-                multiple = FALSE,
-                accept = c("text/csv",
-                           "text/comma-separated-values,text/plain",
-                           ".csv")),
-      # Input: Checkbox if file has header ----
-      checkboxInput("fileuploadHeader", "Header vorhanden?", TRUE),
-      # Input: Select separator ----
-      radioButtons("fileuploadSep", "Separator auswählen:",
-                   choices = c(Semicolon = ";",
-                               Comma = ",",
-                               Tab = "\t"),
-                   selected = ","),
-      # Horizontal line ----
-      tags$hr(),
-      p(h4("Daten eingrenzen")),
-      # DateRange
-      dateRangeInput("daterange", 
-                      "Zeitspanne auswählen:",
-                      start  = "2018-01-01",
-                      end    = "2019-07-01",
-                      min    = "2016-01-01",
-                      max    = "2024-12-31",
-                      format = "yy-mm-dd",
-                      separator = " bis "),
-      # Checkbox
-      checkboxInput("filterhours", "Auf stündliche Werte begrenzen? (Speed-Up)", FALSE),
-      verbatimTextOutput("filterhours"),
-      # Horizontal line ----
-      tags$hr(),
-      downloadButton('downloadData', 'Daten herunterladen'),
-      # Horizontal line ----
-      tags$hr(),
-      # Div mit Copyrights         
-      tags$div(
-        tags$p("Javan Rasokat (79133)"),
-        tags$p("Daniel Böhm (76477)"),       
-        img(src="bee_small.jpg", width="200px"))
-      
-    ),
-    mainPanel(tabsetPanel(
-      tabPanel("Data", dataTableOutput("tabelle")),
-      tabPanel("Histogramm Gewicht", plotOutput("distPlot")),
-      tabPanel("Summary", dataTableOutput("summary")),
-      tabPanel("Corr", textOutput("cor")),
-      tabPanel("Example", plotOutput("firsttry")),
-      tabPanel("About", uiOutput("about"))
-    ))
-    
-    
-    
-  )
-))
+shinyUI(
+      navbarPage("Bienenstand Shiny App",
+                 tabPanel("Start",
+      fluidPage(
+        sidebarLayout(
+          sidebarPanel(
+            p(h4("Eigenes File hochladen")),
+            fileInput("fileuploadFile", "CSV-Datei auswählen:",
+                      multiple = FALSE,
+                      accept = c("text/csv",
+                                 "text/comma-separated-values,text/plain",
+                                 ".csv")),
+            # Input: Checkbox if file has header ----
+            checkboxInput("fileuploadHeader", "Header vorhanden?", TRUE),
+            # Input: Select separator ----
+            radioButtons("fileuploadSep", "Separator auswählen:",
+                         choices = c(Semicolon = ";",
+                                     Comma = ",",
+                                     Tab = "\t"),
+                         selected = ","),
+            # Horizontal line ----
+            tags$hr(),
+            p(h4("Daten eingrenzen")),
+            # DateRange
+            dateRangeInput("daterange", 
+                            "Zeitspanne auswählen:",
+                            start  = "2018-01-01",
+                            end    = "2019-07-01",
+                            min    = "2016-01-01",
+                            max    = "2024-12-31",
+                            format = "yy-mm-dd",
+                            separator = " bis "),
+            # Checkbox
+            checkboxInput("filterhours", "Auf stündliche Werte begrenzen? (Speed-Up)", FALSE),
+            verbatimTextOutput("filterhours"),
+            # Horizontal line ----
+            tags$hr(),
+            downloadButton('downloadData', 'Daten herunterladen'),
+            # Horizontal line ----
+            tags$hr(),
+            # Div mit Copyrights         
+            tags$div(
+              tags$p("Javan Rasokat (79133)"),
+              tags$p("Daniel Böhm (76477)"),       
+              img(src="bee_small.jpg", width="200px"))
+            
+          ),
+          mainPanel(tabsetPanel(
+            tabPanel("Data", dataTableOutput("tabelle")),
+            tabPanel("Histogramm Gewicht", plotOutput("distPlot")),
+            tabPanel("Summary", dataTableOutput("summary")),
+            tabPanel("Corr", textOutput("cor")),
+            tabPanel("Example", plotOutput("firsttry")),
+            tabPanel("About", uiOutput("about"))
+          ))
+        )
+      ))
+      )
+)
