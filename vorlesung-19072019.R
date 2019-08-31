669d263bfd5dfd3dbd5786b2700f31e49ccda8b0
Javan Rasokat <mail@javan.de>
2019-08-31 20:03:56 +0200

    Rename Vorlesung-19072019.R to vorlesung-19072019.R

--- vorlesung-19072019.R
+++ vorlesung-19072019.R
@@ -5,7 +5,7 @@ chisq.test(x)
 #data: x
 #x-squared = 5,1136, df = 2, p-value = 0.07755
 
-#Praxis R - Beispielcode für Korrelationsberechnungen
+#Praxis R - Beispielcode fÃ¼r Korrelationsberechnungen
 #View(mtcars)
 #cor(mtcars$mpg,mtcars$hp)
 #-0.7761684 (pearson ist default)
@@ -21,47 +21,47 @@ chisq.test(x)
 #corrgram(mtcars)
 #blau Korrelation
 #rot keine Korrleation
-#Größe gibt an, ob stärker Korrelation oder nicht
-#Interessant für eine kurze Übersicht der vorhandenen Korrelationen
+#GrÃ¶ÃŸe gibt an, ob stÃ¤rker Korrelation oder nicht
+#Interessant fÃ¼r eine kurze Ãœbersicht der vorhandenen Korrelationen
 #Bspw. disp und wt aus mtcars
 #ggplot(mtcars)+geom_point(aes(x=disp,y=wt))
 #Multivariate Analyse
 #ggplot(mtcars)+geom_point(aes(x=disp,y=wt,size=cyl))
 #ggplot(mtcars)+geom_point(aes(x=disp,y=wt,col=cyl))
 #ggplot(mtcars)+geom_point(aes(x=disp,y=wt,col=as.factor(cyl)))
-#letztere ggf. besser Darstellung, da davor mehr Zylinder suggeriert werden können (bspw. 7, etc.)
+#letztere ggf. besser Darstellung, da davor mehr Zylinder suggeriert werden kÃ¶nnen (bspw. 7, etc.)
 
 #ggplot(mtcars)+geom_point(aes(x=disp,y=wt,col=as.factor(cyl),size=mpg))
-#Ausreißer oben rechts ersichtlich.
+#AusreiÃŸer oben rechts ersichtlich.
 
 
 #Lineare Regression = lm()
 #lm(data=mtcars,mpg~hp) data legt den Datensatz fest, $spaltenname nicht mehr notwendig
 #fit<-lm(data=mtcars,mpg~hp)
-#summary(fit) für mehr Informationen / auch interessant für Lineares Modell
-#Multiple R-squared [0,1], 1=gut/passt 0=nicht (Bestimmtheitsmaß)
+#summary(fit) fÃ¼r mehr Informationen / auch interessant fÃ¼r Lineares Modell
+#Multiple R-squared [0,1], 1=gut/passt 0=nicht (BestimmtheitsmaÃŸ)
 
 #plot(data=mtcars,mpg~hp)
-#abline(fit) "häßlicher Plot"
-#Daher über ggplot
+#abline(fit) "hÃ¤ÃŸlicher Plot"
+#Daher Ã¼ber ggplot
 #ggplot(mtcars, aes(x=hp,y=mpg))+geom_point()
 #ggplot(mtcars, aes(x=hp,y=mpg))+geom_point()+geom_smooth()
 #ggplot(mtcars, aes(x=hp,y=mpg))+geom_point()+geom_smooth(method="lm)
-#Eher nächster Plot, da sonst "unangenehme" Fragen bzw. gefährliches Halbwissen
+#Eher nÃ¤chster Plot, da sonst "unangenehme" Fragen bzw. gefÃ¤hrliches Halbwissen
 #ggplot(mtcars, aes(x=hp,y=mpg))+geom_point()+geom_smooth(method="lm", se = FALSE)
 
 #Splitten der Linie sinnvoll?
 #ggplot(mtcars)+geom_point(aes(x=hp,y=mpg,col=factor(cyl)))
 #Ja aufgrund des Einflussfaktor cyl
 #ggplot(mtcars)+geom_point(aes(x=hp,y=mpg,col=factor(cyl)))+geom_smooth(aes(x=hp,y=mpg,col=factor(cyl)),method = "lm", se=FALSE)
-#Auch neue Aussagefähigkeit
-#Einschränken möglich:
+#Auch neue AussagefÃ¤higkeit
+#EinschrÃ¤nken mÃ¶glich:
 #ggplot()+geom_point(aes(x=mtcars$hp,y=mtcars$mpg,col=factor(mtcars$cyl)))+geom_smooth(aes(x=subset(mtcars,cyl==4)$hp,y=subset(mtcars,cyl==4)$mpg),method = "lm", se=FALSE)
 #Farben freidefinierbar
 #ggplot()+geom_point(aes(x=mtcars$hp,y=mtcars$mpg,col=factor(mtcars$cyl)))+geom_smooth(aes(x=subset(mtcars,cyl==4)$hp,y=subset(mtcars,cyl==4)$mpg),method = "lm", se=FALSE,col="red")
 #
 
-#Einbinden SQLite-DB für Bestimmtheitsmaß & Co.
+#Einbinden SQLite-DB fÃ¼r BestimmtheitsmaÃŸ & Co.
 #con<-dbConnect(RSQLite::SQLite(),file.choose())
 #Downloads: 2019_06_02_hamster_big.sqlite
 #dbListTables(con)
@@ -79,8 +79,8 @@ chisq.test(x)
 #dbListTables(con)
 #daten<-dbReadTable(con,"rteu_logger_data")
 #
-#str(daten) für einen ersten Überblick.
-#ggplot(daten)+geom_histrogram(aes(timestamp)) Durchsicht der einzelnen Spalten per univariate Analyse // Überlicherweise per Barplot!
+#str(daten) fÃ¼r einen ersten Ãœberblick.
+#ggplot(daten)+geom_histrogram(aes(timestamp)) Durchsicht der einzelnen Spalten per univariate Analyse // Ãœberlicherweise per Barplot!
 #tmp<-as.POSIXct(daten$timestamp,origin="1970-01-01",tz="UTC")
 #head(tmp)
 #ggplot(daten)+geom_histrogram(aes(duration)) Aha, Diskrete Werte
@@ -90,21 +90,21 @@ chisq.test(x)
 #ggplot(daten)+geom_bar(aes(receiver))
 #ggplot(daten)+geom_histrogram(aes(max_signal))
 #ggplot(daten)+geom_histrogram(aes(signal_bw))
-#daten$timestamp<-as.POSIXct(daten$timestamp,origin="1970-01-01") für hübschere Timestamp-Ansicht
+#daten$timestamp<-as.POSIXct(daten$timestamp,origin="1970-01-01") fÃ¼r hÃ¼bschere Timestamp-Ansicht
 #ggplot(daten)+geom_point(aes(x=timestamp,y=max_signal))
 #ggplot(daten)+geom_point(aes(x=timestamp,y=max_signal,col=receiver))
 
-#Zusammenhänge suchen
+#ZusammenhÃ¤nge suchen
 #cor(daten) Fehlermeldung x muss numerisch sein
 #is.numeric(daten) = FALSE
 #cor(daten[,c("duration","signal_freq","max_signal","signal_bw")]) Korrelationsmatrix
-#Großer Zusammenhang zwischen max_signal und signal_bw
+#GroÃŸer Zusammenhang zwischen max_signal und signal_bw
 #ggplot(daten)+geom_point(aes(x=signal_bw,y=max_signal,col=as.factor(duration))
 #ggplot(daten)+geom_point(aes(x=as.factor(duration),y=max_signal,col=signal_bw)) Variablen durchtauschen, um ggf. neue Erkenntnisse rauszuziehen
 #td<-diff(daten$timestamp)
 #ggplot()+geom_histogram(aes(td)) Zeitabstand geht gegen Null, nicht geordnet
 #td<-diff(daten$timestamp[order(daten$timestamp)])
 #ggplot()+geom_histogram(aes(td))
-#ggplot()+geom_point(aes(x=daten$timestamp[-1],y=td)) Interpretation der Zeitabstände // [-1] zur Anpassung der Dimensionen
+#ggplot()+geom_point(aes(x=daten$timestamp[-1],y=td)) Interpretation der ZeitabstÃ¤nde // [-1] zur Anpassung der Dimensionen
 #Daten schreiben / speichern
-#daten$td<-c(0,td) 0 für den ersten Eintrag, td für die restlichen Werte
+#daten$td<-c(0,td) 0 fÃ¼r den ersten Eintrag, td fÃ¼r die restlichen Werte
